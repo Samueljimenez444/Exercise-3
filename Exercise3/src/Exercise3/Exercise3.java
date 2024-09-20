148d94ca0542891313a99048197230fc49655a32
samuel.jimenez <samuel.jimenez@106-15.info2>
2024-09-20 14:01:36 +0200

    Exercise 3 Completed

---
+++ Exercise3/src/Exercise3/Exercise3.java
@@ -0,0 +1,13 @@
+package Exercise3;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		System.out.println("1+2" + "+3" + "= 6");
+		System.out.print("6-4");
+		System.out.println("=2");
+		System.out.println("2+3=5");
+		System.out.print("The final answer is 5");
+	}
+
+}
